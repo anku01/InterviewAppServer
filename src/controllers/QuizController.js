4645b27632e1bb51eb70416f319b26150991fddc
swarup-gaikwad <swarup.gaikwad@kohls.com>
2018-12-26 18:46:30 +0530

    drop collection at the end of exam

--- src/controllers/QuizController.js
+++ src/controllers/QuizController.js
@@ -105,7 +105,7 @@ const submitTestAndGetResult = (req, res) =>{
                 });
             });
         });
-        //  ExamStatModel.collection.drop();
+        ExamStatModel.collection.drop();
          
      
 };
